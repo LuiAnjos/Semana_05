34c13ea3784955cec5ffc003569af441359279b7
LuiAnjos <luiluianjos@gmail.com>
2021-08-30 13:15:23 +0000

    Exercício 01

---
+++ Exercicio01.java
@@ -0,0 +1,22 @@
+package Semana_05;
+public class Exercicio01 {
+    public void EXERCICIO() {
+        double vinte = 1;
+        for(int z = 1; z <= 20; z++) {
+            vinte *= z;
+        }
+        System.out.println(vinte);
+
+        double trinta = 1;
+        for(int x = 1; x <= 30; x++) {
+            trinta *= x;
+        }
+        System.out.println(trinta);
+
+        double quarenta = 1;
+        for(int y = 1; y <= 40; y++) {
+            quarenta *= y;
+        }
+        System.out.println(quarenta);
+    }
+}
--- package Semana_05;.java
+++ package Semana_05;.java
@@ -3,9 +3,9 @@ import java.util.Scanner;
 
 public class principal {
     public static void main(String[] args) {
-        exercicio01 e1 = new exercicio01();
-        exercicio02 e2 = new exercicio02();
-        exercicio03 e3 = new exercicio03();
+        Exercicio01 e1 = new exercicio01();
+        Exercicio02 e2 = new exercicio02();
+        Exercicio03 e3 = new exercicio03();
 
         e1.EXERCICIO();
         e2.EXERCICIO();
