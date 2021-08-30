d555b2436f0d761d60903cd3d7fd1e1be65649dc
LuiAnjos <luiluianjos@gmail.com>
2021-08-30 13:10:28 +0000

    Principal

---
+++ package Semana_05;.java
@@ -0,0 +1,14 @@
+package Semana_05;
+import java.util.Scanner;
+
+public class principal {
+    public static void main(String[] args) {
+        exercicio01 e1 = new exercicio01();
+        exercicio02 e2 = new exercicio02();
+        exercicio03 e3 = new exercicio03();
+
+        e1.EXERCICIO();
+        e2.EXERCICIO();
+        e3.EXERCICIO();
+    }
+}
